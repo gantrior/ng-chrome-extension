39c0f1239d1c570f24b0ac9aa8f9ae28340f8dc2
Tomas Panik <tomas.panik@covered.lht>
2018-06-26 23:23:15 +0200

    experimental

--- content-script/boot.ts
+++ content-script/boot.ts
@@ -1,4 +1,25 @@
+import * as _ from 'lodash';
+import * as axios from 'axios';
+import * as $ from 'jquery';
+
 import { ConnectListener } from './connect-listener';
 import { RuntimeListener } from './runtime-listener';
+
+const test = 'asdf';
+const xxx = _.includes(test, 'aaa');
+
+console.log(test);
+console.log(xxx);
+
+$( 'h1' ).css( 'border', '9px solid red' );
+
+axios.default.get('https://jsonplaceholder.typicode.com/posts/1')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
 console.log('content script!');
 const runtimeListener = new RuntimeListener();
